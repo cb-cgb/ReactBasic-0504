2abba3809dd753bf05c4efdbe85dfa15023f7586
Chana Becker <cgoldzm@gmail.com>
2021-02-03 19:57:31 -0500

    Add project files.

---
+++ ReactExercise5-4/ClientApp/src/MyFirstComponent.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import MyOtherComponent from './MyOtherComponent';
+
+class MyFirstComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>from MyFirstComponent: Hello, how are you today?</h1>
+                <MyOtherComponent />
+            </div>
+        );
+    }
+}
+
+export default MyFirstComponent;
---
+++ ReactExercise5-4/ClientApp/src/MyOtherComponent.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+class MyOtherComponent extends React.Component {
+    render() {
+        return <p>Today is a beautiful day with snow covering the ground but streets are clear. </p>;
+    }
+}
+
+export default MyOtherComponent;
